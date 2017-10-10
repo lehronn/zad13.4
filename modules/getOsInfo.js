ccfd349aee45a9fa60ceb7fd19ad0c5be7e4fc14
Mateusz Stomski <lehronn@gmail.com>
2017-10-10 20:45:56 +0200

    first commit

---
+++ modules/getOsInfo.js
@@ -0,0 +1,28 @@
+var os = require('os');
+var timeFormat = require('./timeFormat');
+
+function getOSinfo() {
+  var type = os.type();
+  var release = os.release();
+  var CPU = os.cpus()[0].model;
+  var uptime = os.uptime();
+  var userInfo = os.userInfo();
+  
+  if(type === 'Darwin') {
+    type = 'OSX';
+  } 
+      else if(type === 'Windows_NT') {
+        type = 'Windows';
+      }
+  console.log('\nSystem:', type);
+  console.log('Release:', release);
+  console.log('CPU: ' + CPU);
+  console.log('User name:', userInfo.username);
+  console.log('Home dir:', userInfo.homedir);
+  console.log('====\nUptime:');
+  timeFormat.print(uptime);
+  
+  //console.log('====\nUptime:', timeFormat.print(uptime));
+}
+
+exports.print = getOSinfo;
